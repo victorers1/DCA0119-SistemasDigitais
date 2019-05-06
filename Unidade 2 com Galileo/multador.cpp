ac455bb206bb3c92b54eb11151f500dbcac474ad
Victor Emanuel <victorers1@hotmail.com>
2019-05-05 22:15:29 -0300

    Galileo lê 2 LDRs

---
+++ Unidade 2 com Galileo/multador.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+
+/* mraa headers */
+#include "mraa/aio.hpp"
+#include "mraa/common.hpp"
+
+/* AIO ports */
+#define AIO_PORT0 0
+#define AIO_PORT1 1
+
+using namespace std;
+
+volatile sig_atomic_t flag = 1;
+
+void sig_handler(int signum) {
+  if (signum == SIGINT) {
+    cout << "Exiting..." << std::endl;
+    flag = 0;
+  }
+}
+
+int main(void) {
+  uint16_t LDR0, LDR1;
+  
+  signal(SIGINT, sig_handler);
+
+  /* initialize AIO */
+  mraa::Aio ldr0(AIO_PORT0);
+  mraa::Aio ldr1(AIO_PORT1);
+
+  while (flag) {
+    LDR0 = ldr0.read();
+    LDR1 = ldr1.read();
+    cout<<"LDR0: "<<LDR0<<edl;
+    cout<<"LDR1: "<<LDR1<<edl;
+  }
+
+  return EXIT_SUCCESS;
+}
